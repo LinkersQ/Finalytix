37e24da300e1a685da1d9363987abd9016976f8b
Aleksey Shepelkin <a.shepelkin@mail.ru>
2023-01-10 19:01:25 +0300

    Добавлен проект по парсингу свечных файлов из исторической выюорки

---
+++ ParseCandles/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ParseCandles
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string parrentPath = "D:\\data";
+            string[] files = Directory.GetFiles(parrentPath);
+            print(files.Count().ToString());
+
+            
+        }
+
+        private static void print(string message, bool isError)
+        {
+            if (isError)
+                Console.WriteLine("{0} ERROR: {1}", DateTime.Now, message);
+            else
+                Console.WriteLine("{0} INFO: {1}", DateTime.Now, message);
+        }
+        private static void print(string message)
+        {
+            Console.WriteLine("{0} INFO: {1}", DateTime.Now, message);
+        }
+    }
+}
